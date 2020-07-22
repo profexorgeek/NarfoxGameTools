65daacb7257abe4ed1819690e582efdf85c58946
Justin Johnson <justin.d.johnson@gmail.com>
2020-07-22 10:35:02 -0600

    Started artpack

---
+++ NarfoxStudiosArtPack.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="NarfoxStudiosArtPack" tilewidth="32" tileheight="32" tilecount="16384" columns="128">
+ <image source="NarfoxStudiosArtPack.png" width="4096" height="4096"/>
+ <terraintypes>
+  <terrain name="TopDownIslands" tile="768"/>
+ </terraintypes>
+ <tile id="768" terrain="0,0,0,0" probability="0.01"/>
+ <tile id="769" terrain="0,0,0,0"/>
+ <tile id="770" terrain="0,0,0," probability="0.1"/>
+ <tile id="771" terrain="0,0,,0" probability="0.1"/>
+ <tile id="772" terrain=",,0,0"/>
+ <tile id="773" terrain=",,0,0"/>
+ <tile id="774" terrain=",,0,0"/>
+ <tile id="775" terrain=",,0,0"/>
+ <tile id="776" terrain="0,0,0," probability="0.1"/>
+ <tile id="777" terrain="0,0,,0" probability="0.1"/>
+ <tile id="778" terrain="0,0,0,"/>
+ <tile id="779" terrain="0,0,,0"/>
+ <tile id="896" terrain="0,0,0,0"/>
+ <tile id="897" terrain="0,0,0,0"/>
+ <tile id="898" terrain="0,,0,0" probability="0.1"/>
+ <tile id="899" terrain=",0,0,0" probability="0.1"/>
+ <tile id="900" terrain="0,0,,"/>
+ <tile id="901" terrain="0,0,,"/>
+ <tile id="902" terrain="0,0,,"/>
+ <tile id="903" terrain="0,0,,"/>
+ <tile id="904" terrain="0,,0,0" probability="0.1"/>
+ <tile id="905" terrain=",0,0,0" probability="0.1"/>
+ <tile id="906" terrain="0,,0,0"/>
+ <tile id="907" terrain=",0,0,0"/>
+ <tile id="1028" terrain=",0,,0"/>
+ <tile id="1029" terrain="0,,0,"/>
+ <tile id="1030" terrain=",,,0" probability="0.2"/>
+ <tile id="1031" terrain=",,0," probability="0.2"/>
+ <tile id="1032" terrain=",,,0"/>
+ <tile id="1033" terrain=",,0,"/>
+ <tile id="1156" terrain=",0,,0"/>
+ <tile id="1157" terrain="0,,0,"/>
+ <tile id="1158" terrain=",0,," probability="0.2"/>
+ <tile id="1159" terrain="0,,," probability="0.2"/>
+ <tile id="1160" terrain=",0,,"/>
+ <tile id="1161" terrain="0,,,"/>
+ <tile id="1284" terrain=",0,,0"/>
+ <tile id="1285" terrain="0,,0,"/>
+ <tile id="1286" terrain=",,,0" probability="0.1"/>
+ <tile id="1287" terrain=",,0," probability="0.1"/>
+ <tile id="1288" terrain=",,,0"/>
+ <tile id="1289" terrain=",,0,"/>
+ <tile id="1412" terrain=",0,,0"/>
+ <tile id="1413" terrain="0,,0,"/>
+ <tile id="1414" terrain=",0,," probability="0.1"/>
+ <tile id="1415" terrain="0,,," probability="0.1"/>
+ <tile id="1416" terrain=",0,,"/>
+ <tile id="1417" terrain="0,,,"/>
+</tileset>
