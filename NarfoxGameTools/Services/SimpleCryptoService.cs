7f6635a1912d88bc6167a6f49e2fa7cc3c862484
Justin Johnson <justin.d.johnson@gmail.com>
2020-09-25 12:21:05 -0600

    Ported the simple crypto service from Masteroid

---
+++ NarfoxGameTools/Services/SimpleCryptoService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NarfoxGameTools.Services
+{
+    /// <summary>
+    /// This class SHOULD NOT be used for sensitive data. This provides
+    /// really easy encryption and decryption of strings as an extension
+    /// method. This is useful for light encryption/decryption of game
+    /// data.
+    /// </summary>
+    public static class SimpleCryptoService
+    {
+        private static byte[] key;
+        private static byte[] iv;
+        private static bool initialized;
+        private static SymmetricAlgorithm algo;
+
+        /// <summary>
+        /// The key used to encrypt and decrypt messages. The
+        /// key is considered secret in that it is used to
+        /// decrypt data
+        /// </summary>
+        public static string KeyString
+        {
+            get
+            {
+                if (key != null)
+                {
+                    return Convert.ToBase64String(key);
+                }
+                return "";
+            }
+            set
+            {
+                key = Convert.FromBase64String(value);
+            }
+        }
+
+        /// <summary>
+        /// The Initialization Vector, a random string that is used
+        /// to "salt" a message before encryption. The Initialization
+        /// Vector is typically considered non-secret.
+        /// </summary>
+        public static string IvString
+        {
+            get
+            {
+                if (iv != null)
+                {
+                    return Convert.ToBase64String(iv);
+                }
+                return "";
+            }
+            set
+            {
+                iv = Convert.FromBase64String(value);
+            }
+        }
+
+        public static void Initialize(string encKey, string encVector)
+        {
+            algo = DES.Create();
+            KeyString = encKey;
+            IvString = encVector;
+            initialized = true;
+        }
+
+        /// <summary>
+        /// An extension method on the string object that allows strings
+        /// to be encrypted on the fly by calling myString.Encrypt().
+        /// Note that the SimpleCryptoService must be initialized with a
+        /// key and initialization vector or an exception will be thrown.
+        /// </summary>
+        /// <param name="str">The unencrypted string to encrypt</param>
+        /// <returns>An encrypted string</returns>
+        public static string Encrypt(this string str)
+        {
+            if (!initialized)
+            {
+                throw new Exception("Crypto system has not been initialized!");
+            }
+
+            ICryptoTransform transform = algo.CreateEncryptor(key, iv);
+            byte[] inputBuffer = Encoding.UTF8.GetBytes(str);
+            byte[] outputBuffer = transform.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+            return Convert.ToBase64String(outputBuffer);
+        }
+
+        /// <summary>
+        /// An extension method on the string object that allows strings
+        /// to be decrypted on the fly by calling myString.Decrypt().
+        /// Note that the SimpleCryptoService must be initialized with a
+        /// key and initialization vector or an exception will be thrown.
+        /// </summary>
+        /// <param name="str">The encrypted string to decrypt</param>
+        /// <returns>A decrypted string</returns>
+        public static string Decrypt(this string str)
+        {
+            if (!initialized)
+            {
+                throw new Exception("Crypto system has not been initialized!");
+            }
+
+            ICryptoTransform transform = algo.CreateDecryptor(key, iv);
+            byte[] inputBuffer = Convert.FromBase64String(str);
+            byte[] outputBuffer = transform.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+            return Encoding.UTF8.GetString(outputBuffer);
+        }
+    }
+}
