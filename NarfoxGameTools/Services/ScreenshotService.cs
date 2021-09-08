06b2964eb1349273a24ac3e38391ae955ef93a15
Justin Johnson <justin.d.johnson@gmail.com>
2021-09-07 19:05:45 -0600

    Minor usings cleanup

--- NarfoxGameTools/Services/ScreenshotService.cs
+++ NarfoxGameTools/Services/ScreenshotService.cs
@@ -2,12 +2,9 @@
 using FlatRedBall.Graphics;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace NarfoxGameTools.Services
 {
