14ae6b99b46ed9fabbfa0ea24eb75c54866936b5
Justin Johnson <justin.d.johnson@gmail.com>
2022-07-07 18:21:22 -0600

    Added ISaveModel interface for versioned saves

---
+++ NarfoxGameTools/Data/ISaveModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NarfoxGameTools.Data
+{
+    /// <summary>
+    /// A version of a ViewModel that can be persisted to disk.
+    /// It should always contain a version and date. The Value field
+    /// can be used to convert the save to some cash value in game if
+    /// the object can not be deserialized in the current game version
+    /// </summary>
+    public interface ISaveModel
+    {
+        /// <summary>
+        /// The game version that this was saved under
+        /// </summary>
+        string GameVersion { get; set; }
+
+        /// <summary>
+        /// The value of this item in case it cannot be serialized.
+        /// Allows the game to grant in-game value for items that
+        /// have been removed from the game
+        /// </summary>
+        float ItemValue { get; set; }
+
+        /// <summary>
+        /// The date this item was saved. Should usually be set
+        /// before serialized to disk!
+        /// </summary>
+        DateTime SaveDate { get; set; }
+    }
+}
