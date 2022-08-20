9957c767cb7523ef567af26ac3eb2198a9f55589
Justin Johnson <justin.d.johnson@gmail.com>
2022-08-20 09:44:41 -0600

    Added some cursor extensions

---
+++ NarfoxGameTools/Extensions/CursorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FlatRedBall;
+using FlatRedBall.Gui;
+
+namespace NarfoxGameTools.Extensions
+{
+    public static class CursorExtensions
+    {
+        public static void CenterOnScreen(this Cursor cursor)
+        {
+            cursor.ScreenX = (int)(Camera.Main.DestinationRectangle.Width / 2.0f);
+            cursor.ScreenY = (int)(Camera.Main.DestinationRectangle.Height / 2.0f);
+        }
+    }
+}
