57015ec200038df36b37dacb8ab810f35045ca88
Justin Johnson <justin.d.johnson@gmail.com>
2025-04-03 08:54:27 -0700

    NOTE: algo change see description
    Added XML comments, some sanity checks, and improved naming. Changed the random point in rectangle method away from an old Masteroid-specific method. This will break things but also changed the name to be specific.

--- NarfoxGameTools/Extensions/RandomExtensions.cs
+++ NarfoxGameTools/Extensions/RandomExtensions.cs
@@ -5,21 +5,39 @@ namespace NarfoxGameTools.Extensions
 {
     public static class RandomExtensions
     {
+        /// <summary>
+        /// A float-precision version of PI
+        /// </summary>
         public static float Pi => (float)Math.PI;
 
-
+        /// <summary>
+        /// Generates a random float between min and max
+        /// </summary>
+        /// <param name="rand">The random instance to use</param>
+        /// <param name="min">The inclusive minimum value</param>
+        /// <param name="max">The exclusive maximum value</param>
+        /// <returns>A float within the provided range</returns>
         public static float InRange(this Random rand, double min, double max)
         {
             return rand.InRange((float)min, (float)max);
         }
+
+        /// <summary>
+        /// Generates a random float between min and max
+        /// </summary>
+        /// <param name="rand">The random instance to use</param>
+        /// <param name="min">The inclusive minimum value</param>
+        /// <param name="max">The exclusive maximum value</param>
+        /// <returns>A float within the provided range</returns>
         public static float InRange(this Random rand, float min, float max)
         {
-            // early out for equal. This may not be perfectly accurate
-            // but it makes no difference, all it's doing is saving
-            // a calculation
+            // early out for equal, we return min because the return value is
+            // supposed to be min inclusive but max exclusive. So returning min
+            // is slightly more semantically accurate but the values are identical
+            // so it doesn't actually matter.
             if (min == max)
             {
-                return max;
+                return min;
             }
 
             var range = max - min;
@@ -27,46 +45,86 @@ namespace NarfoxGameTools.Extensions
             return min + randInRange;
         }
 
-        public static Vector3 PositionInRadius(this Random rand, float radius)
+        /// <summary>
+        /// Generates a random position within a radius with adjustment for
+        /// even distribution
+        /// </summary>
+        /// <param name="rand">The random instance to use</param>
+        /// <param name="radius">The radius to generate points within</param>
+        /// <returns>A Vector2 position</returns>
+        public static Vector2 PositionInRadius(this Random rand, float radius)
         {
-            var vector = Vector3.Zero;
             var coefficient = (float)Math.Sqrt(rand.NextDouble());
             var magnitude = coefficient * radius;
             var angle = rand.InRange(0, 2 * Pi);
-            vector.X = (float)(Math.Cos(angle) * magnitude);
-            vector.Y = (float)(Math.Sin(angle) * magnitude);
+
+            var vector = new Vector2(
+                (float)(Math.Cos(angle) * magnitude),
+                (float)(Math.Sin(angle) * magnitude));
             return vector;
         }
 
-        public static Vector3 PositionInRectangle(this Random random, float sectorSize, float minZ, float maxZ)
+        /// <summary>
+        /// Generates a random position within a 3D cube with 0 at center
+        /// </summary>
+        /// <param name="rand">The random instance to use</param>
+        /// <param name="cubeSize">The size of the cuber</param>
+        /// <returns>A random Vector3 within the cube</returns>
+        public static Vector3 PositionInCube(this Random rand, float cubeSize)
         {
-            var halfSize = sectorSize / 2f;
+            var halfSize = cubeSize / 2f;
             return new Vector3(
-                random.InRange(-halfSize, halfSize),
-                random.InRange(-halfSize, halfSize),
-                random.InRange(minZ, maxZ));
+                rand.InRange(-halfSize, halfSize),
+                rand.InRange(-halfSize, halfSize),
+                rand.InRange(-halfSize, halfSize));
         }
 
+        /// <summary>
+        /// Generates a random positive or negative sign
+        /// </summary>
+        /// <param name="rand">The random instance to use</param>
+        /// <returns></returns>
         public static float Sign(this Random rand)
         {
             return rand.NextDouble() < 0.5 ? 1f : -1f;
         }
 
-        public static Color Color(this Random rand, float minTint = 0f, float maxTint = 1f)
+        /// <summary>
+        /// Generates a random Color with an optional maximum and minimum tint
+        /// </summary>
+        /// <param name="rand">The random instance to use</param>
+        /// <param name="minIntensity">The maximum channel intensity between 0-1 where 0 is pure black</param>
+        /// <param name="maxIntensity">The minimum channel intensity between 0-1 where 1 is pure white</param>
+        /// <returns>A random color where each channel is within the provided intensity</returns>
+        public static Color Color(this Random rand, float minIntensity = 0f, float maxIntensity = 1f)
         {
-            var r = rand.InRange(minTint, maxTint);
-            var g = rand.InRange(minTint, maxTint);
-            var b = rand.InRange(minTint, maxTint);
+            minIntensity = minIntensity.ClampTo(0f, 1f);
+            maxIntensity = maxIntensity.ClampTo(0f, 1f);
+
+            var r = rand.InRange(minIntensity, maxIntensity);
+            var g = rand.InRange(minIntensity, maxIntensity);
+            var b = rand.InRange(minIntensity, maxIntensity);
 
             return new Color(r, g, b);
         }
 
+        /// <summary>
+        /// Chooses a random value within an enum
+        /// </summary>
+        /// <typeparam name="T">The type of enum</typeparam>
+        /// <param name="rand">The random instance to use</param>
+        /// <returns>A random value from the enum</returns>
         public static T EnumValue<T>(this Random rand)
         {
             T[] vals = Enum.GetValues(typeof(T)) as T[];
             return vals.Random(rand);
         }
 
+        /// <summary>
+        /// Generates a random boolean
+        /// </summary>
+        /// <param name="rand">The random instance to use</param>
+        /// <returns>True or false at random</returns>
         public static bool Bool(this Random rand)
         {
             if(rand.NextDouble() < 0.5)
