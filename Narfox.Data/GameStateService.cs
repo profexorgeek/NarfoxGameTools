e7c63acba6d6a2026662612f57297534f51edce3
Justin Johnson <justin.d.johnson@gmail.com>
2025-04-14 22:46:23 -0600

    Getting close but still struggling with overwriting states in the same frame

--- Narfox.Data/GameStateService.cs
+++ Narfox.Data/GameStateService.cs
@@ -3,6 +3,7 @@ using Narfox.Data.Models;
 using Narfox.Extensions;
 using System.Diagnostics;
 using System.Reflection;
+using System.Threading.Channels;
 
 namespace Narfox.Data
 {
@@ -27,25 +28,24 @@ namespace Narfox.Data
         /// This class is used internally to package up model
         /// change requests with metadata for a queue
         /// </summary>
-        private class QueueModelAction
+        private class ModelChangeRequest
         {
+            public ushort Id { get; set; }
             public ActionType Action { get; set; }
-            public IEntityModel Model { get; set; }
             public Client Requestor { get; set; }
-            
+            public Dictionary<string, object> ChangedProperties { get; set; }
 
-            public QueueModelAction(IEntityModel model, ActionType action, Client requestor)
+            public ModelChangeRequest(ushort id, ActionType action, Client requestor, Dictionary<string, object> changes)
             {
-                Model = model;
+                Id = id;
                 Action = action;
                 Requestor = requestor;
+                ChangedProperties = changes;
             }
         }
 
 
         List<IEntityModel> trackedModels;
-        List<QueueModelAction> modelChangeQueue;
-        Dictionary<string, float> lerpProperties;
 
 
         /// <summary>
@@ -77,12 +77,6 @@ namespace Narfox.Data
         /// </summary>
         public Client Authority { get; private set; }
 
-        /// <summary>
-        /// How close two numeric values must be to be considered "the same". Helps
-        /// prevent forever lerping by amounts that may be smaller than float precision.
-        /// </summary>
-        public float LerpCompleteThreshold { get; set; } = 0.1f;
-
 
 
         /// <summary>
@@ -92,8 +86,6 @@ namespace Narfox.Data
         public GameStateService(Client localClient = null)
         {
             trackedModels = new List<IEntityModel>();
-            modelChangeQueue = new List<QueueModelAction>();
-            lerpProperties = new Dictionary<string, float>();
 
             if (localClient == null)
             {
@@ -121,267 +113,123 @@ namespace Narfox.Data
         }
 
         /// <summary>
-        /// Registers a property name on models that should always lerp over several frames
-        /// when merging models. NOTE: only numeric properties can lerp!
+        /// Requests changes to an existing model.
         /// </summary>
-        /// <param name="propertyName">The property name to lerp</param>
-        /// <param name="lerpPerFrame">The amount of lerp to apply each frame</param>
-        public void RegisterLerpProperty(string propertyName, float lerpPerFrame = 0.2f)
+        /// <param name="id">The unique ID of the model to be changed</param>
+        /// <param name="requestor">The change requestor</param>
+        /// <param name="changes">A key value list of changes, the value type must match the target model.</param>
+        public void RequestUpdateModel(ushort id, Client requestor, Dictionary<string, object> changes)
         {
-            if(lerpProperties.ContainsKey(propertyName))
-            {
-                lerpProperties[propertyName] = lerpPerFrame;
-            }
-            else
-            {
-                lerpProperties.Add(propertyName, lerpPerFrame);
-            }
-        }
-
-        /// <summary>
-        /// Registers multiple lerp properties at once.
-        /// </summary>
-        /// <param name="lerps">A dictionary of property names and lerp amount per frame</param>
-        public void RegisterLerpProperty(Dictionary<string, float> lerps)
-        {
-            foreach(var key in lerps.Keys)
-            {
-                RegisterLerpProperty(key, lerps[key]);
-            }
-        }
-
-        /// <summary>
-        /// Enqueues a request to perform some action on a provided model and tracks
-        /// the requestor. Models added to a queue will be merged with existing models
-        /// with any lerpable properties being lerped over time and removed from the
-        /// queue once changes are fully applied.
-        /// </summary>
-        /// <param name="updatedModel">An IEntityModel representing the new state</param>
-        /// <param name="action">The type of action, such as create, update, or delete</param>
-        /// <param name="requestor">The requesting client, whose authority over the model will be checked</param>
-        public void TryEnqueueModelChange(IEntityModel updatedModel, ActionType action, Client requestor)
-        {
-
-
-
-            // Are we the authority of this model?
-
-
-            // If so, we can create/update/delete immediately without queing
-
-            // We only need to queue
-
-
-
-            // EARLY OUT: for a request to be honored, either the message must be a reckoning issued by the
-            // authority OR the requestor must be the owner of the updated model
-            if(updatedModel.OwnerId != requestor.Id &&
-                requestor.Id != Authority.Id)
+            var model = trackedModels.FirstOrDefault(m => m.Id == id);
+            
+            // EARLY OUT: can't change unknown model
+            if(model == null)
             {
                 return;
             }
 
-            // add or update the model to the queue
-            var alreadyEnqueued = modelChangeQueue.Where(q => q.Model.Id == updatedModel.Id &&
-            q.Action == action).FirstOrDefault();
-            if(alreadyEnqueued == null)
-            {
-                modelChangeQueue.Add(new QueueModelAction(updatedModel, action, requestor));
-            }
-            else
-            {
-                alreadyEnqueued.Model = updatedModel;
-            }
-        }
-
-        /// <summary>
-        /// Processes model changes waiting in the change queue. Should be called in a main
-        /// tick or engine loop
-        /// </summary>
-        public virtual void Update()
-        {
-            for (var i = modelChangeQueue.Count - 1; i > -1; i--)
-            {
-                var q = modelChangeQueue[i];
-                bool processingComplete = false;
-                switch (q.Action)
-                {
-                    case ActionType.None:
-                        // NOOP
-                        break;
-                    case ActionType.Create:
-                        processingComplete = CreateQueuedItem(q);
-                        break;
-                    case ActionType.Update:
-                        processingComplete = UpdateQueuedItem(q);
-                        break;
-                    case ActionType.Destroy:
-                        processingComplete = DestroyQueuedItem(q);
-                        break;
-                    case ActionType.Reckon:
-                        processingComplete = ReckonQueuedItem(q);
-                        break;
-                }
-
-                // if this item is fully processed, we remove it from the queue
-                // some items may lerp over many frames
-                if(processingComplete)
-                {
-                    modelChangeQueue.RemoveAt(i);
-                }
-            }
-        }
-
-
-
-        /// <summary>
-        /// If the queued model doesn't already exist in our tracked models, we add
-        /// it and raise the ModelAdded event for any subscribers.
-        /// </summary>
-        /// <param name="q">A queue item</param>
-        /// <returns>True if this item has been fully processed and can be removed from the queue</returns>
-        bool CreateQueuedItem(QueueModelAction q)
-        {
-            var existing = trackedModels.FirstOrDefault(t => t.Id == q.Model.Id);
-            if (existing == null)
-            {
-                trackedModels.Add(q.Model);
-                ModelAdded?.Invoke(q.Requestor, new EntityModelEventArgs(q.Model));
-            }
-            else
-            {
-                // NOOP: likely this is a stale request
-            }
-
-            // there's no lerping or stalling here so we always return true
-            // because this should be fully processed
-            return true;
-        }
-
-        /// <summary>
-        /// Updates an existing model by looping through each of its properties and setting the
-        /// existing model's properties to matching properties from the queued model.
-        /// 
-        /// If the property name exists in the list of registered properties to interpolate, it
-        /// will interpolate the values instead of directly setting them. The queued item will
-        /// stay in the queue until the lerping process has reached the threshold defined by
-        /// the LerpCompleteThreshold
-        /// </summary>
-        /// <param name="q">A queued model to update</param>
-        /// <returns>True if this item has been fully processed and can be removed from the queue</returns>
-        bool UpdateQueuedItem(QueueModelAction q)
-        {
-            var existing = trackedModels.FirstOrDefault(t => t.Id == q.Model.Id);
-
-            // EARLY OUT: no model found, it may have been deleted so let's consider
-            // this processing complete
-            if(existing == null)
-            {
-                return true;
-            }
-
-            // EARLY OUT: models are the same reference or the new one is null
-            if(q.Model == null || q.Model == existing)
-            {
-                Debug.Assert(false, "Models were null or the same reference!");
-                return true;
-            }
 
             // reflect properties
-            var properties = existing.GetType().GetProperties(
+            var properties = model.GetType().GetProperties(
                 BindingFlags.Public |
                 BindingFlags.Instance);
-
-            bool isComplete = true;
 
             // set all properties on the existing model to match the target model, using reflection
             // ensures that we don't break references to our list of registered entities
             foreach (var prop in properties)
             {
-                // ignore properties we can't change
-                if (prop.CanRead == false || 
+                // ignore properties we can't or shouldn't change
+                if (prop.CanRead == false ||
                     prop.CanWrite == false ||
                     prop.Name == nameof(IEntityModel.Id) ||
-                    prop.Name == nameof(IEntityModel.OwnerId))
+                    prop.Name == nameof(IEntityModel.OwnerId) ||
+                    prop.Name == nameof(IEntityModel.EntityTypeName))
                     continue;
 
-                var existingValue = prop.GetValue(existing);
-                var targetValue = prop.GetValue(q.Model);
-
-                // we can lerp if both values are valid and the property type is numeric
-                if(lerpProperties.ContainsKey(prop.Name) &&
-                    existingValue != null &&
-                    targetValue != null &&
-                    MathExtensions.IsNumericType(prop.PropertyType))
+                foreach (var key in changes.Keys)
                 {
-                    var lerpedValue = MathExtensions.LerpNumeric(existingValue, targetValue, lerpProperties[prop.Name]);
-                    prop.SetValue(existing, lerpedValue);
-
-                    // if our new value is still too far away from the target value, we mark this
-                    // as incomplete so the model stays in queue
-                    double delta = MathExtensions.GetDelta(lerpedValue, targetValue);
-                    if(delta > LerpCompleteThreshold)
+                    if (prop.Name == key)
                     {
-                        isComplete = false;
+                        prop.SetValue(model, changes[key]);
                     }
                 }
-
-                // otherwise we just snap to the new value
-                if(isComplete)
-                {
-                    prop.SetValue(existing, targetValue);
-                }
             }
-
-            return isComplete;
         }
 
         /// <summary>
-        /// Removes a registered model if it exists and raises the destroyed event
+        /// Requests a model to be created
         /// </summary>
-        /// <param name="q">A queued model to delete</param>
-        /// <returns>True if this item has been fully processed and can be removed from the queue</returns>
-        bool DestroyQueuedItem(QueueModelAction q)
+        /// <param name="model">The model to add to the tracked models</param>
+        /// <param name="requestor">The requestor</param>
+        public void RequestCreateModel(IEntityModel model, Client requestor)
         {
-            var existing = trackedModels.FirstOrDefault(t => t.Id == q.Model.Id);
+            var exists = trackedModels.Any(t => t.Id == model.Id);
+            if(exists == false && model.OwnerId == requestor.Id)
+            {
+                trackedModels.Add(model);
+                ModelAdded?.Invoke(requestor, new EntityModelEventArgs(model));
+            }
+        }
 
-            // remove the model from tracked models
-            if (existing != null)
+        /// <summary>
+        /// Requests that a model be deleted.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="requestor"></param>
+        public void RequestDeleteModel(ushort id, Client requestor)
+        {
+            var existing = trackedModels.FirstOrDefault(t => t.Id == id);
+            if(existing != null && existing.OwnerId == requestor.Id)
             {
                 trackedModels.Remove(existing);
-
-                var ea = new EntityModelEventArgs(existing);
-                ModelDestroyed?.Invoke(q.Requestor, ea);
+                var args = new EntityModelEventArgs(existing);
+                ModelDestroyed?.Invoke(requestor, args);
             }
-
-            // NOTE: we should consider removing any other queued actions for this
-            // entity ID so that no actions try to execute on an entity that has
-            // been destroyed. However, we can't do that here because we're in the
-            // process of walking the queue. Updating will noop if the model doesn't
-            // exist but rapid create/destroy may result in out-of-sequence actions
-            // being taken.
-
-            return true;
         }
 
         /// <summary>
-        /// Updates a matching model if it exists, creates it if it does not.
+        /// Forces an existing model to match an incoming model. Will not change
+        /// the existing model's reference, it will shallow copy all properties
+        /// except the core identifying properties defined in the IEntityModel
         /// </summary>
-        /// <param name="q">A queued model to update</param>
-        /// <returns>True if this item has been fully processed and can be removed from the queue</returns>
-        bool ReckonQueuedItem(QueueModelAction q)
+        /// <param name="id">The ID of the target model</param>
+        /// <param name="requestor">The reckoning requestor</param>
+        /// <param name="incomingModel">The model to match</param>
+        public void RequestReckonModel(ushort id, Client requestor, IEntityModel incomingModel)
         {
-            var existing = trackedModels.FirstOrDefault(t => t.Id == q.Model.Id);
-            if (existing == null)
+            var existing = trackedModels.FirstOrDefault(t => t.Id == id);
+            if(existing != null && requestor.Id == Authority.Id && incomingModel.GetType() == existing.GetType())
             {
-                CreateQueuedItem(q);
-            }
-            else
-            {
-                UpdateQueuedItem(q);
-            }
+                // reflect properties
+                var properties = existing.GetType().GetProperties(
+                    BindingFlags.Public |
+                    BindingFlags.Instance);
 
-            return true;
+                // set all properties on the existing model to match the target model, using reflection
+                // ensures that we don't break references to our list of registered entities
+                foreach (var prop in properties)
+                {
+                    // ignore properties we can't or shouldn't change
+                    if (prop.CanRead == false ||
+                        prop.CanWrite == false ||
+                        prop.Name == nameof(IEntityModel.Id) ||
+                        prop.Name == nameof(IEntityModel.OwnerId) ||
+                        prop.Name == nameof(IEntityModel.EntityTypeName))
+                        continue;
+
+                    var newValue = prop.GetValue(incomingModel);
+                    prop.SetValue(existing, newValue);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Should be called in the main engine loop, performs any state service updates
+        /// </summary>
+        public virtual void Update()
+        {
+            
         }
     }
 }
